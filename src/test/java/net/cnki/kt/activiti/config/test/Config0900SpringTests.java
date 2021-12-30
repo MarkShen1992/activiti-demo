f22b05921c24bcc86e1dcf90b57b134207cc162f
shenjy <markshen1992@gmail.com>
2021-12-30 22:09:44 +0800

    refactoring and add some new functions.

---
+++ src/test/java/net/cnki/kt/activiti/config/test/Config0900SpringTests.java
@@ -0,0 +1,56 @@
+package net.cnki.kt.activiti.config.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.ActivitiRule;
+import org.activiti.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:activiti-context.xml"})
+public class Config0900SpringTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(Config0900SpringTests.class);
+
+    @Rule
+    @Autowired
+    public ActivitiRule activitiRule;
+
+    @Autowired
+    private RuntimeService runtimeService;
+
+    @Autowired
+    private TaskService taskService;
+
+    @Test
+    @Deployment(resources = {"my-process.bpmn20.xml"})
+    public void testConfigSpring1() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
+        assertNotNull(processInstance);
+        Task task = taskService.createTaskQuery().singleResult();
+        assertEquals("Activiti is awesome!", task.getName());
+        taskService.complete(task.getId());
+    }
+
+    @Test
+    @Deployment(resources = {"my-process_spring.bpmn20.xml"})
+    public void testConfigSpring2() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("my-process");
+        assertNotNull(processInstance);
+        Task task = taskService.createTaskQuery().singleResult();
+        assertEquals("Activiti is awesome!", task.getName());
+        taskService.complete(task.getId());
+    }
+}
